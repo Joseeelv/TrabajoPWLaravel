3fc1773e7b9bfc61e872d20ae8270c7ca37e83e2
MadaoSurmanito <alejandro.sanzhuerta@alum.uca.es>
2025-05-06 10:26:27 +0200

    Contacto listo

--- Kebab/routes/web.php
+++ Kebab/routes/web.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 })->name('home');
-
+Route::view('/contact', 'contact')->name('contact');
 
 Route::get('/register', [RegisterController::class, 'create'])->name('register');
 Route::post('/register', [RegisterController::class, 'register']);
